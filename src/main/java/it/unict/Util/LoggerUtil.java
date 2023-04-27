bc616d9c093d836c9bc369ae49bcba5330824a92
Giacomo Rotondo Cocco <grotondococco@gmail.com>
2023-04-28 01:35:45 +0200

    ✨ 🔊 add Log Utils

---
+++ src/main/java/it/unict/Util/LoggerUtil.java
@@ -0,0 +1,15 @@
+package it.unict.Util;
+
+import org.slf4j.Logger;
+
+public class LoggerUtil {
+    public static void logMethodStart(Logger log){
+        StackTraceElement[] stackTrace = new Throwable().getStackTrace();
+        log.debug("{} - Method START", stackTrace[1]);
+    }
+
+    public static void logMethodEnd(Logger log){
+        StackTraceElement[] stackTrace = new Throwable().getStackTrace();
+        log.debug("{} - Method START", stackTrace[1]);
+    }
+}
