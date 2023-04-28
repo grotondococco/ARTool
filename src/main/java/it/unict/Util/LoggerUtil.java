dbb2a928c4e1eea162ed24f756d52e6f3af65dbd
Giacomo Rotondo Cocco <grotondococco@gmail.com>
2023-04-28 18:04:30 +0200

    🔊 chore: improve logs legibility

--- src/main/java/it/unict/Util/LoggerUtil.java
+++ src/main/java/it/unict/Util/LoggerUtil.java
@@ -3,13 +3,14 @@ package it.unict.Util;
 import org.slf4j.Logger;
 
 public class LoggerUtil {
-    public static void logMethodStart(Logger log){
+
+    public static void logMethodStart(Logger log) {
         StackTraceElement[] stackTrace = new Throwable().getStackTrace();
-        log.debug("{} - Method START", stackTrace[1]);
+        log.debug("{} - Method START <------>", stackTrace[1]);
     }
 
-    public static void logMethodEnd(Logger log){
+    public static void logMethodEnd(Logger log) {
         StackTraceElement[] stackTrace = new Throwable().getStackTrace();
-        log.debug("{} - Method END", stackTrace[1]);
+        log.debug("{} - Method END <------>", stackTrace[1]);
     }
 }
