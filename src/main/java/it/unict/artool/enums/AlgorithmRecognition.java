8279eaccf77f072c2d1a83e796d741df9914c5fa
Giacomo Rotondo Cocco <grotondococco@gmail.com>
2023-07-07 01:57:35 +0200

    typo fix

--- src/main/java/it/unict/artool/enums/AlgorithmRecognition.java
+++ src/main/java/it/unict/artool/enums/AlgorithmRecognition.java
@@ -3,7 +3,7 @@ package it.unict.artool.enums;
 public enum AlgorithmRecognition {
     CONDITIONAL("CONDITIONAL: Check multiple If Statements over the same left expression and operator then convert it into a Switch Statement."),
     ARRAY("ARRAY: Convert Arrays usage with List usage."),
-    LIST("LIST: Covert LinkedList in ArrayList."),s
+    LIST("LIST: Covert LinkedList in ArrayList."),
     SET("SET: Convert TreeSet in HashSet."),
     SINGLEMETHODCALL("SINGLEMETHODCALL: Transfers the body of methods called once at the calling position.");
     private final String description;
